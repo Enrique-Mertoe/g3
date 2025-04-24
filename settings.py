f70a565a97b9d3195ee102d382109fe64551895f
abuti <abutimartin778@gmail.com>
2025-04-24 04:48:27 +0300

    c1

--- settings.py
+++ settings.py
@@ -4,6 +4,6 @@ from pathlib import Path
 BASE_PATH = Path(os.path.dirname(os.path.abspath(__file__)))
 VPN_DIR = Path("/etc/openvpn")
 
-ALLOWED_ORIGINS = ['https://yourdomain.com', 'http://127.0.0.1:5000', "http://localhost:5173"]
+ALLOWED_ORIGINS = ['https://isp3.lomtechnology.com', 'http://127.0.0.1:5000', "http://localhost:5173"]
 
-CORS_TRUSTED_ORIGINS = ["https://yourdomain.com", "http://localhost:5173"]
+CORS_TRUSTED_ORIGINS = ["https://isp3.lomtechnology.com", "http://localhost:5173"]
